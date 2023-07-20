8741c4bda3cf884942c1e75d269de273df00f908
Moses <mochengo29@gmail.com>
2023-07-20 14:14:18 +0300

    Completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [addedToCart, setAddedToCart] = useState(false);
+
+  function handleAddToCart() {
+    setAddedToCart((add) => !add);
+  }
+  // { textDecoration: { "line-through" : ''} }
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button
+        onClick={handleAddToCart}
+        className={addedToCart ? "in-cart remove" : "add"}
+      >
+        {addedToCart ? "Remove From Cart" : "Add To Cart"}
+      </button>
     </li>
   );
 }
